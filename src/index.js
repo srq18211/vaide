d94103aca285b2b9b479feade36ceb38ee8afc3d
srq18211 <37665471+srq18211@users.noreply.github.com>
2020-11-12 17:51:34 +0800

    A 新增 图片说明 打印路由对应文件位置信息

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import create from '@/tools/create'
-import Vaide from './vaide.vue'
+import create from '@/tools/create';
+import Vaide from './vaide.vue';
 
 export default {
-  install: function (Vue, router) {
-    Vue.prototype.$vaide = create(Vaide, { router})
+  install: function (Vue, router, options = { log: true }) {
+    if (process.env.NODE_ENV !== 'development') {
+      return
+    }
+    Vue.prototype.$vaide = create(Vaide, router)
+    const { log } = options
+    if (log) {
+      console.log(
+        `%c Vaide %c ${'version 2.0.2'} %c`,
+        'background:#35495e; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+        // 'background:#008c8c ; padding: 1px; border-radius: 0 3px 5px 0;  color: #fff',
+        'background:#1890ff ; padding: 1px; border-radius: 0 3px 5px 0;  color: #fff',
+        // 'background:#ff5f10 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
+        'background:transparent'
+      )
+      router.resolveHooks.push((to, from, next) => {
+        var str = `          ____ Vaide: currentRoute matched ____          \n\n`
+        to.matched.forEach(item => {
+          str += `【${item.path}】:\n__filePath__: ${item.components.default.__file} \n\n`
+        })
 
-    console.log(
-      `%c Vaide %c ${'Vue DevAide 1.0.0'} %c`,
-      'background:#35495e; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
-      // 'background:#008c8c ; padding: 1px; border-radius: 0 3px 5px 0;  color: #fff',
-      'background:#1890ff ; padding: 1px; border-radius: 0 3px 5px 0;  color: #fff',
-      // 'background:#ff5f10 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
-      'background:transparent'
-    )
+        console.log(`%c${str}`, 'padding:1px;color: #008c8c;text-align:right');
+        next()
+      })
+    }
+
   }
 }
